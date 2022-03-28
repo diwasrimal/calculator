40d56c8cf4de1c06a8f5c82a112310d50a0417c2
Diwas_lufy <84910758+diwasrimal@users.noreply.github.com>
2022-03-28 23:52:49 +0545

    modify main.js

--- main.js
+++ main.js
@@ -1,30 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
     let operations = document.getElementsByClassName("btns")
-    let operands = document.getElementsByClassName('box')
-    let operand1 = Number(operands[0].value)
-    let operand2 = Number(operands[1].value)
-
+    var result
+    
     for (let i = 0; i < operations.length; i++) {
-        let operation = operations[i]
+        let button = operations[i]
 
         // Adding a 'click' listener for each operation
-        operation.addEventListener('click', () => {
-            switch (operation) {
+        button.addEventListener('click', () => {
+            
+            // Reading value of operands
+            var operands = document.getElementsByClassName('box')
+            var operand1 = Number(operands[0].value)
+            var operand2 = Number(operands[1].value)
+
+            // button.value could be (+, - , *, /)
+            switch (button.value) {
                 case "+":
-                    console.log(operand1 + operand2)
+                    result = operand1 + operand2
                     break
                 case "-":
-                    console.log(operand1 - operand2)
+                    result = operand1 - operand2
                     break
                 case "*":
-                    console.log(operand1 * operand2)
+                    result = operand1 * operand2
                     break
                 case "÷":
-                    console.log(operand1 / operand2)
+                    result = operand1 / operand2
                     break
                 default:
-                    return
+                    alert("No operation selected!")
             }
         })
+
     }
+        let acAndEqualButtons = document.getElementsByClassName("ac-and-equal")
+        let acBtn = acAndEqualButtons[0]
+        let equalBtn = acAndEqualButtons[1]
+
+        equalBtn.addEventListener('click', () => {
+            document.querySelector("#result").innerHTML = result
+        })
+        
+        acBtn.addEventListener('click', () => {
+            document.querySelector("#result").innerHTML = ''
+            document.getElementsByClassName('box')[0].innerHTML = ''
+            document.getElementsByClassName('box')[1].innerHTML = ''
+        })
+    
 })
