cc1e6842c6f49b2de6b815d9c8f1ae4a1b9a7f98
Diwas_lufy <84910758+diwasrimal@users.noreply.github.com>
2022-03-29 23:01:48 +0545

    use a little js

--- main.js
+++ main.js
@@ -1,50 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let operations = document.getElementsByClassName("btns")
-    var result
-    
-    for (let i = 0; i < operations.length; i++) {
-        let button = operations[i]
+    console.log("content loaded")
 
-        // Adding a 'click' listener for each operation
-        button.addEventListener('click', () => {
-            
-            // Reading value of operands
-            var operands = document.getElementsByClassName('box')
-            var operand1 = Number(operands[0].value)
-            var operand2 = Number(operands[1].value)
+    let buttons = document.getElementsByTagName('button')
 
-            // button.value could be (+, - , *, /)
-            switch (button.value) {
-                case "+":
-                    result = operand1 + operand2
-                    break
-                case "-":
-                    result = operand1 - operand2
-                    break
-                case "*":
-                    result = operand1 * operand2
-                    break
-                case "÷":
-                    result = operand1 / operand2
-                    break
-                default:
-                    alert("No operation selected!")
-            }
-        })
+    console.log(buttons[3].value)
+    console.log(buttons.length)
 
+    for (let i = 0; i < buttons.length; i++) {
+        console.log(buttons[i].value)
     }
-        let acAndEqualButtons = document.getElementsByClassName("ac-and-equal")
-        let acBtn = acAndEqualButtons[0]
-        let equalBtn = acAndEqualButtons[1]
 
-        equalBtn.addEventListener('click', () => {
-            document.querySelector("#result").innerHTML = result
-        })
-        
-        acBtn.addEventListener('click', () => {
-            document.querySelector("#result").innerHTML = ''
-            document.getElementsByClassName('box')[0].innerHTML = ''
-            document.getElementsByClassName('box')[1].innerHTML = ''
-        })
-    
 })
